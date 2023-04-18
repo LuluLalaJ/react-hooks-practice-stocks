808b3f888bb71dea3b300352bda609493dfdd694
Chen <chen.jiang.1206@gmail.com>
2023-04-18 15:49:37 -0400

    render all stocks

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState, useEffect }from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const stocksUrl = "http://localhost:3001/stocks/"
+  const [stocks, setStocks] = useState([])
+  useEffect(()=>{
+    fetch(stocksUrl)
+    .then(r => r.json())
+    .then(data => setStocks(data))
+  }, [])
+
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocks} />
         </div>
         <div className="col-4">
           <PortfolioContainer />
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Stock from "./Stock";
+import { render } from "react-dom";
 
-function StockContainer() {
+function StockContainer({ stocks }) {
+  const renderStocks = stocks.map( stock => <Stock stock={stock} />)
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {renderStocks}
     </div>
   );
 }
